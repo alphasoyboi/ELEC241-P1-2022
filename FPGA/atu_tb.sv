d3efe08037ef433de6d8df61796fa8899527256e
JasonHoare <72444346+JasonHoare@users.noreply.github.com>
2022-05-04 11:49:23 +0100

    started ATU testbench

---
+++ FPGA/atu_tb.sv
@@ -0,0 +1,11 @@
+module atu_tb;
+
+//internal signals
+logic CLK = 0;
+logic hall_1, hall_2, reset, monitor, clockwise;
+logic [11:0] angle;
+
+//instantiate ATU
+angle_tracking_unit u1(angle, hall_1, hall_2, reset, monitor, clockwise);
+
+endmodule
