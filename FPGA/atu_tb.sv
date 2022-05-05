97b0c46e3beacbc2664b6fd3a95824e79733574b
JasonHoare <72444346+JasonHoare@users.noreply.github.com>
2022-05-05 17:51:48 +0100

    added a simulated input from hall sensors for both clockwise and anticlockwise motor rotation

--- FPGA/atu_tb.sv
+++ FPGA/atu_tb.sv
@@ -2,12 +2,42 @@ module atu_tb;
 
 //internal signals
 logic CLK = 0;
-logic hall_1, hall_2, reset, monitor, clockwise;
+logic hall_1 = 0;
+logic hall_2 = 0;
+logic reset, monitor, clockwise;
 logic [11:0] angle;
+logic [1:0] counter = 2'd0;
 
 //instantiate ATU
 angle_tracking_unit u1(angle, hall_1, hall_2, reset, monitor, clockwise);
 
 always #50ps CLK = ~CLK;
+
+always_ff @(posedge CLK) begin
+	if(clockwise == 1'b1) begin
+		if(counter == 2'd0) begin
+			hall_2 = ~hall_2;
+			counter++;
+		end
+		else if(counter == 2'd3) begin
+			hall_1 = ~hall_1;
+			counter = 2'd0;
+		end
+		else
+			counter++;
+	end
+	else begin
+		if(counter == 2'd0) begin
+			hall_1 = ~hall_1;
+			counter++;
+		end
+		else if(counter == 2'd3) begin
+			hall_2 = ~hall_2;
+			counter = 2'd0;
+		end
+		else
+			counter++;
+	end
+end
 
 endmodule
