dfaf4ba3c296db455931e79873823676a6e62032
alphasoyboi <67895142+alphasoyboi@users.noreply.github.com>
2022-05-19 22:49:48 +0100

    added button interrupts converted to c strings
    realized i might be able to use stl strings instead which is much cleaner and not really costly for what i'm doing

--- MCU/ELEC241-P1-2022/main.cpp
+++ MCU/ELEC241-P1-2022/main.cpp
@@ -13,15 +13,16 @@ DigitalOut cs(PC_6);       // chip select
 static BufferedSerial serial_port(USBTX, USBRX);
 // module support board buttons a & b
 static InterruptIn button_a(PG_0), button_b(PG_1);
-
-uint16_t spi_readwrite(uint16_t data);
+DigitalOut tx_led(LED1);
 
 typedef enum {
+    INIT,
     READ_CMD,
     PROC_CMD
 } TermState;
 
 static const char *help_msg = R"(
+
 commands:
     help   [angle|period|ctrl|cmd|power]
     angle  [0..360]
@@ -38,11 +39,24 @@ commands:
     cmd    [cont|zero|brake] - send command [toggle continuous mode|reset zero angle|toggle brake]
     power  [on|off]          - set pwm power
 )";*/
-static const char *inv_cmd_msg = "invalid command (type help for commands and values)\n";
-static const char *inv_val_msg = "inavlid value (type help for commands and values)\n";
+static const char *inv_cmd_msg = "\ninvalid command (type help for commands and arguments)\n";
+static const char *inv_arg_msg = "\ninavlid value (type help for commands and arguments)\n";
 
+uint16_t spi_readwrite(uint16_t data);
+TermState term_read(TermState state);
 
 int main() {
+    //SET UP THE SPI INTERFACE
+    cs = 1;                     // Chip must be deselected, Chip Select is active LOW
+    spi.format(16,0);           // Setup the DATA frame SPI for 16 bit wide word, Clock Polarity 0 and Clock Phase 0 (0)
+    spi.frequency(1000000);     // 1MHz clock rate
+    wait_us(10000);
+    // This will hold the 16-bit data returned from the SPI interface (sent by the FPGA)
+    // Currently the inputs to the SPI recieve are left floating (see quartus files)
+    uint16_t rx;
+    uint16_t tx;
+
+    // set up terminal interface
     serial_port.set_baud(9600);
     serial_port.set_format(
         /* bits */     8,
@@ -52,66 +66,31 @@ int main() {
     // turn off blocking read/writes
     serial_port.set_blocking(false);
 
-    //SET UP THE SPI INTERFACE
-    cs = 1;                     // Chip must be deselected, Chip Select is active LOW
-    spi.format(16,0);           // Setup the DATA frame SPI for 16 bit wide word, Clock Polarity 0 and Clock Phase 0 (0)
-    spi.frequency(1000000);     // 1MHz clock rate
-    wait_us(10000);
+    int target_angle = 0, new_target_angle = 0;
 
-    // This will hold the 16-bit data returned from the SPI interface (sent by the FPGA)
-    // Currently the inputs to the SPI recieve are left floating (see quartus files)
-    uint16_t rx;
+    // assign interrupts
+    button_a.rise([&new_target_angle](){
+        ++new_target_angle;
+        if (new_target_angle > 360)
+            new_target_angle = 0;
+    });
+    button_b.rise([&new_target_angle](){
+        --new_target_angle;
+        if (new_target_angle < 0)
+            new_target_angle = 360;
+    });
 
-    TermState term_state = READ_CMD;
-    char c = 0;
-    std::string buf;
+    
+    TermState term_state = INIT;
     serial_port.write(help_msg, strlen(help_msg));
     while(true)                 
     {    
-        switch (term_state) {
-            case READ_CMD: {
-                if (serial_port.read(&c, 1) != -EAGAIN) {
-                    serial_port.write(&c, 1);
-                    if (c == '\r') {
-                        c = '\n';
-                        serial_port.write(&c, 1);
-                        c = 0;
-                        term_state = PROC_CMD;
-                    } else if (c) {
-                        buf += c;
-                    }
-                }
-                break;
-            }
-            case PROC_CMD: {
-                if (buf.find("help") == 0) {
-                    serial_port.write(help_msg, strlen(help_msg));
-                } 
-                else if (buf.find("angle") == 0) {
-                    int angle = -1;
-                    angle = stoi(buf.substr(buf.find_first_of(" ") + 1));
-                    if (angle < 0 || angle > 360)
-                        serial_port.write(inv_val_msg, strlen(inv_val_msg));
-                }
-                else if (buf.find("period") == 0) {
-                    int period = -1;
-                    period = stoi(buf.substr(buf.find_first_of(" ") + 1));
-                    if (period < 0 || period > 255)
-                        serial_port.write(inv_val_msg, strlen(inv_val_msg));
-                }
-                else if (buf.find("ctrl") == 0) {
-                    
-                }
-                else {
-                    serial_port.write(inv_cmd_msg, strlen(inv_cmd_msg));
-                }
-
-                buf.erase();
-                term_state = READ_CMD;
-                break;
-            }
-            default:
-                term_state = READ_CMD;
+        term_state = term_read(term_state);
+        if (target_angle != new_target_angle) {
+            target_angle = new_target_angle;
+            tx = ANGLE << 12;
+            tx += target_angle;
+            rx = spi_readwrite(tx);
         }
 
         /*
@@ -140,4 +119,59 @@ uint16_t spi_readwrite(uint16_t data) {
 	cs = 1;             									//De-Select the device by seting chip select HIGH
 	wait_us(1);
 	return rx;
+}
+
+TermState term_read(TermState state)
+{
+    char c = 0;
+    static size_t i = 0;
+    constexpr size_t BUF_LEN = 64;
+    static char buf[BUF_LEN];
+
+    switch (state) {
+        case READ_CMD: {
+            if (serial_port.read(&c, 1) != -EAGAIN) {
+                serial_port.write(&c, 1);
+                if (c == '\r' || i >= BUF_LEN) {
+                    i = 0;
+                    buf[BUF_LEN - 1] = '\0';
+                    state = PROC_CMD;
+                } else if (c) {
+                    buf[i++] = c;
+                }
+            break;
+        }
+        case PROC_CMD: {
+            char *p = nullptr;
+            if ((p = strstr(buf, "help")) == buf) {
+                serial_port.write(help_msg, strlen(help_msg));
+            } 
+            else if ((p = strstr(buf, "angle")) == buf) {
+                int angle = -1;
+                if ((p = strchr(buf, ' ')))
+                    angle = atoi(p);
+                if (angle < 0 || angle > 360)
+                    serial_port.write(inv_arg_msg, strlen(inv_arg_msg));
+            }
+            else if ((p = strstr(buf, "period")) == buf) {
+                int period = -1;
+                if((p = strchr(buf, ' ')))
+                    period = atoi(p);
+                if (period < 0 || period > 255)
+                        serial_port.write(inv_arg_msg, strlen(inv_arg_msg));
+            }
+            else {
+                serial_port.write(inv_cmd_msg, strlen(inv_cmd_msg));
+            }
+
+            memset(buf, '\0', BUF_LEN - 1);
+            state = READ_CMD;
+            break;
+        }
+        default: {
+            memset(buf, '\0', BUF_LEN - 1);
+            state = READ_CMD;
+        }
+    }
+    return state;
 }
