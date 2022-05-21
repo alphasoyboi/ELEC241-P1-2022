6f2a919cc0423650eae1976fa14ac08f333da31e
JasonHoare <72444346+JasonHoare@users.noreply.github.com>
2022-05-21 23:28:12 +0100

    added some inputs and outputs to SCU, added internal registers

--- FPGA/servo_controller_unit.sv
+++ FPGA/servo_controller_unit.sv
@@ -1,14 +1,11 @@
-module servo_controller_unit (
-    output logic [7:0] motor_period, motor_duty, 
-    output logic [1:0] motor_ctrl, 
-    output logic [31:0] status_reg, 
-	 input logic [31:0] input_reg,
-	 input logic clk, n_reset
-);
+module servo_controller_unit (output logic [7:0] motor_period, motor_duty, output logic [1:0] motor_ctrl, output logic [31:0] status_reg, output logic [11:0] displayAngle, output logic atuReset, atuMonitor, clockwise, brake, pwmOn, input logic [31:0] input_reg, input logic [11:0] currentAngle, input logic clk, n_reset);
 
 assign angle = 12'd1006;
 assign motor_period = 20;
 assign motor_duty = 255;
 assign motor_ctrl = 2;
+
+logic [31:0] inputInternal;
+logic [31:0] statusInternal;
 
 endmodule
