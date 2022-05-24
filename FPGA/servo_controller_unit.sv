d03ceeec97b088beab1700d8fdad79112f626b43
alphasoyboi <67895142+alphasoyboi@users.noreply.github.com>
2022-05-24 10:50:10 +0100

    updated var names to naming convention

--- FPGA/servo_controller_unit.sv
+++ FPGA/servo_controller_unit.sv
@@ -1,13 +1,13 @@
 module servo_controller_unit (
-    output logic [7:0] motor_period, motor_duty,
+    output logic [7:0] pwm_period, pwm_duty,
     output logic [31:0] status_reg,
-    output logic atuReset, atuMonitor, clockwise, brake, pwmOn, 
+    output logic atu_reset, atu_monitor, clockwise, pwm_brake, pwm_power, 
     input logic [31:0] input_reg, 
-    input logic [11:0] currentAngle, 
+    input logic [11:0] current_angle, 
     input logic clk, n_reset
 );
 
-assign atuMonitor = 1;
+assign atu_monitor = 1;
 
 logic [31:0] inputInternal;
 logic [31:0] statusInternal = 0;
@@ -19,13 +19,13 @@ int clockwiseBounds, overflowBounds;
 int propUpper, propLower, propOverUpper, propOverLower;
 
 assign status_reg = statusInternal;
-assign motor_period = pwmPeriod;
-assign motor_duty = pwmDuty;
-assign pwmOn = power;
+assign pwm_period = pwmPeriod;
+assign pwm_duty = pwmDuty;
+assign pwm_power = power;
 
 always_ff @(posedge clk) begin
 	inputInternal = input_reg;
-	statusInternal[11:0] = currentAngle;
+	statusInternal[11:0] = current_angle;
 	currentCommand = inputInternal[30:29];
 	if(currentCommand == 2'b00 || currentCommand != lastCommand) begin
 		desiredAngle = inputInternal[11:0];
@@ -33,15 +33,15 @@ always_ff @(posedge clk) begin
 		controlMode = inputInternal[28:27];
 		power = inputInternal[31];
 		if(currentCommand == 2'b10 || currentCommand == 2'b11)
-			brake = 0;
+			pwm_brake = 0;
 		else
-			brake = 1;
+			pwm_brake = 1;
 		if(currentCommand == 2'b01) begin
 			for(int i = 0; i < 0; i++) begin
 				if(i == 0)
-					atuReset = 0;
+					atu_reset = 0;
 				else
-					atuReset = 1;
+					atu_reset = 1;
 			end
 		end
 		
@@ -53,12 +53,12 @@ always_ff @(posedge clk) begin
 		clockwiseBounds = 1006;
 	end
 	else begin
-		if((currentAngle > desiredAngle && currentAngle < clockwiseBounds) || (currentAngle < desiredAngle && currentAngle < overflowBounds))
+		if((current_angle > desiredAngle && current_angle < clockwiseBounds) || (current_angle < desiredAngle && current_angle < overflowBounds))
 			clockwise = 0;
 		else
 			clockwise = 1;
 		if(controlMode == 2'b00) begin
-			if(currentAngle == desiredAngle)
+			if(current_angle == desiredAngle)
 				pwmDuty = 8'd0;
 			else
 				pwmDuty = 8'd255;
@@ -76,15 +76,15 @@ always_ff @(posedge clk) begin
 				propOverLower = propLower + 1006;
 				propLower = 0;
 			end
-			if(currentAngle > desiredAngle && currentAngle < propUpper)
-				pwmDuty = currentAngle - desiredAngle;
-			else if(currentAngle < desiredAngle && currentAngle < propOverUpper)
-				pwmDuty = (8'd255 - propOverUpper) + currentAngle;
-			else if(currentAngle > desiredAngle && currentAngle >= propOverLower)
-				pwmDuty = (currentAngle - propOverLower) + desiredAngle;
-			else if(currentAngle < desiredAngle && currentAngle >= propLower)
-				pwmDuty = 8'd255 - (currentAngle - propLower);
-			else if(currentAngle == desiredAngle)
+			if(current_angle > desiredAngle && current_angle < propUpper)
+				pwmDuty = current_angle - desiredAngle;
+			else if(current_angle < desiredAngle && current_angle < propOverUpper)
+				pwmDuty = (8'd255 - propOverUpper) + current_angle;
+			else if(current_angle > desiredAngle && current_angle >= propOverLower)
+				pwmDuty = (current_angle - propOverLower) + desiredAngle;
+			else if(current_angle < desiredAngle && current_angle >= propLower)
+				pwmDuty = 8'd255 - (current_angle - propLower);
+			else if(current_angle == desiredAngle)
 				pwmDuty = 8'd0;
 			else
 				pwmDuty = 8'd255;
